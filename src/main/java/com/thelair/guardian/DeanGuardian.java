9c179aed37f8cb0ad3ecc272bfdda74b4b939aa3
IBEN ANOOS <iben.anoos@gmail.com>
2025-10-05 20:43:32 +0800

    completed the guardian files

---
+++ src/main/java/com/thelair/guardian/DeanGuardian.java
@@ -0,0 +1,12 @@
+package main.java.com.thelair.guardian;
+
+public class DeanGuardian extends Guardian {
+    public DeanGuardian() {
+        super("Dean", 4, 1000, 70, 25, 50);
+    }
+
+    public String getIntro() {
+        return "Stage 4 – The Hall of Balance: 'Strength with discipline is power.'";
+    }
+
+}
